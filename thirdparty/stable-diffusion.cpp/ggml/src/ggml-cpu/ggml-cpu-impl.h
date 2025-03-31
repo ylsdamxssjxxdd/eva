56fe014d780d9b674c9473b58c21f304298fd81d
sjxx <321389626@qq.com>
2025-03-31 14:01:14 +0800

    继续完善对飞腾cpu的支持

--- thirdparty/stable-diffusion.cpp/ggml/src/ggml-cpu/ggml-cpu-impl.h
+++ thirdparty/stable-diffusion.cpp/ggml/src/ggml-cpu/ggml-cpu-impl.h
@@ -287,6 +287,14 @@ inline static uint8x16_t ggml_vqtbl1q_u8(uint8x16_t a, uint8x16_t b) {
 #define ggml_int8x16x2_t  int8x16x2_t
 #define ggml_int8x16x4_t  int8x16x4_t
 
+// 取消原有定义
+#ifdef vld1q_s8_x4
+#undef vld1q_s8_x4
+#endif
+#ifdef vld1q_u8_x4
+#undef vld1q_u8_x4
+#endif
+
 // 替代vld1q_s8_x4
 inline static int8x16x4_t vld1q_s8_x4(const int8_t *ptr) {
     int8x16x4_t ret;
